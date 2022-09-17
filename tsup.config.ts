16324f3b18ec4f3d3261ee3177eefef480bf2d05
Hezekiah Hendry <hezekiah.hendry@gmail.com>
2022-09-17 19:19:45 -0400

    feat(build): remove `umd` bundle
    BREAKING CHANGE: The `UMD` bundle has been removed

--- tsup.config.ts
+++ tsup.config.ts
@@ -5,14 +5,13 @@ export default defineConfig({
   clean: true,
   dts: false,
   entry: ['src/index.ts'],
-  format: ['esm', 'cjs', 'iife'],
+  format: ['esm', 'cjs'],
   minify: false,
   skipNodeModulesBundle: true,
   sourcemap: true,
   target: 'es2021',
   tsconfig: relative(__dirname, resolve(process.cwd(), 'src', 'tsconfig.json')),
   keepNames: true,
-  globalName: 'PropertyHelpers',
   esbuildOptions(options, context) {
     if (context.format === 'cjs') {
       options.banner = {
